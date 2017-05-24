e57646c530ee785123eaba0a210e822e8fc99bd5
Roman_v <vigmol@ukr.net>
2017-05-24 15:30:02 +0300

    добавил lesson3

---
+++ src/L3/task1/ChetNechet.java
@@ -0,0 +1,19 @@
+package L3.task1;
+import java.util.Scanner;
+public class ChetNechet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        System.out.print("Введите число: ");
+        if (sc.hasNextInt()) {
+            i = sc.nextInt();
+            if ((i % 2) == 0) {
+                System.out.println("Вывод четного числа:");
+            } else {
+                if (i % 1 == 0) System.out.println("Вы ввели нечетное число ");
+            }
+        } else {
+            System.out.println("ОШИБКА");
+        }
+    }
+}
