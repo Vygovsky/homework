142df8cf33244150337a010a2b1bee2850f3d197
Roman_v <vigmol@ukr.net>
2017-05-27 19:07:13 +0300

    добавил рекурсию

---
+++ src/L8/task2/Recursion.java
@@ -0,0 +1,22 @@
+package L8.task2;
+/**
+ * Created by Roman_v on 27.05.2017.
+ */
+public class Recursion {
+    public static void recursion(int a, int b) {
+        if (a < b) {
+            System.out.print(a + " ");
+            recursion(a + 1, b);
+        } else if (a > b) {
+            System.out.print(a + " ");
+            recursion(a - 1, b);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+    public static void main(String[] args) {
+        recursion(12, 45);
+        recursion(45, 12);
+    }
+}
