5b8e596c5f6c9359ed946ec2615e87e745645d02
Roman_v <vigmol@ukr.net>
2017-05-27 21:19:14 +0300

    добавил 7 урок 1,2,3 задание

--- src/L7/task3/Reception.java
+++ src/L7/task3/Reception.java
@@ -2,9 +2,9 @@ package L7.task3;
 
 public class Reception {
     public static void main(String[] args) {
-        User usre = new User("ждлдж", "01/01/1998");
+        User usre = new User(" Раков Артем Иванович", "01/01/1998");
         usre.setPhone(123246546);
-        usre.setFacultet("jkhkjk");
+        usre.setFacultet("КН");
         usre.setIdCard(213);
         usre.takeBook(new Book("Букварь"));
 
