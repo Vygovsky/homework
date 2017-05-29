6ba3a5d57b5661549b9248f9f6fa8b0d0e08dcf0
roman_v <vigmol@ukr.net>
2017-05-29 11:04:16 +0300

    1.Неотформатировано. 2. После объявления переменных обычно идут конструкторы, потом геттеры и сеттеры, потом все остальные методы. Это не критично, но лучше писать так. Исправьте. Исправил #4

--- src/L7/task1/Person.java
+++ src/L7/task1/Person.java
@@ -7,38 +7,15 @@ public class Person {
     private String fullName;
     private int age;
 
-    @Override
-    public String toString() {
-        return "Person{" + "fullName='" + fullName + '\'' + ", age=" + age + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Person person = (Person) o;
-
-        if (age != person.age) return false;
-        return fullName.equals(person.fullName);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = fullName.hashCode();
-        result = 31 * result + age;
-        return result;
-    }
-
     public Person() {
         this.setFullName("Вася");
-
     }
 
     public Person(String Roman, int age) {
         this.setFullName(Roman);
         this.setAge(age);
     }
+
     public void talk() {
         System.out.println("Я " + getFullName() + ". Mне " + getAge() + " лет");
     }
@@ -61,6 +38,27 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "fullName='" + fullName + '\'' + ", age=" + age + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        if (age != person.age) return false;
+        return fullName.equals(person.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fullName.hashCode();
+        result = 31 * result + age;
+        return result;
     }
 }
 
