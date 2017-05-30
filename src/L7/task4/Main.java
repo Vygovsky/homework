006ebc336e9d906542b29bd0c894de36e8d82f8e
roman_v <vigmol@ukr.net>
2017-05-30 10:10:37 +0300

    Добавил Lorry and Sport car в 7 урок task4

--- src/L7/task4/Main.java
+++ src/L7/task4/Main.java
@@ -1,7 +1,9 @@
 package L7.task4;
 
+import L7.task4.com.company.lorry.Lorry;
 import L7.task4.com.company.professions.Driver;
 import L7.task4.com.company.detalis.Engine;
+import L7.task4.com.company.sportcar.SportCar;
 import L7.task4.com.company.vehicles.Car;
 
 /**
@@ -12,7 +14,12 @@ public class Main {
         Car car = new Car("BMW", "X 50M", 2071);
         Engine engine = new Engine(575, "BMW M TWINPOWER TURBO В BMW X5 M");
         Driver driver = new Driver("Кольчин Николай Васьевич", 2.5);
-        car.setD(driver);
+        Lorry lorry = new Lorry("Renault", "MAGNUM", 19000, 30000);
+        Driver driver1 = new Driver("Фаат Андрей Петрович", 5);
+        Engine engine1 = new Engine(480, "DXi 12");
+        SportCar sportCar = new SportCar("Chevrolet Corvette", "Stingray", 1539, 301);
+        Driver driver2 = new Driver("Фернандо Алонсо", 20);
+        car.setD(driver1);
         car.setE(engine);
         car.start();
         car.stop();
