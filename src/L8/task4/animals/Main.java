9dbb7cacccbac97490950708a1321c62e02644ed
roman_v <vigmol@ukr.net>
2017-06-01 14:33:17 +0300

    урок 8 task 4 готов

--- src/L8/task4/animals/Main.java
+++ src/L8/task4/animals/Main.java
@@ -1,9 +1,9 @@
 package L8.task4.animals;
 
-import L8.task4.Vet.Vet;
 import L8.task4.cat.Cat;
 import L8.task4.dog.Dog;
 import L8.task4.horse.Horse;
+import L8.task4.vet.Vet;
 
 /**
  * Created by roman_v on 31.05.17.
@@ -11,13 +11,13 @@ import L8.task4.horse.Horse;
 public class Main {
     public static void main(String[] args) {
         Animals[] animals = {
-                new Cat("Дом", "Корм", 2, 2.5, "Муська", 5),
-                new Cat("Квартира", "Рыба", 5, 3, "Кузя", 8),
-                new Cat("Дом", "Молоко", 12, 12, "Ленин", 3),
-                new Dog("Двор", "Кость", 3, 6, "Шарик", 9),
-                new Dog("Квартира", "Корм для собак", 8, 2, "Номи", 12),
-                new Dog("Квартира", "Все ест", 6, 9, "Арчебальд", 14.5),
-                new Horse("Сарай", "Овес", 15, 450, "Скакун", 300)
+                new Cat("в доме.", "корм", 2, 2.5, "Муська", 5),
+                new Cat("в вартире.", "рыбу", 5, 3, "Кузя", 8),
+                new Cat("в доме.", "молоко", 12, 12, "Ленин", 3),
+                new Dog("во воре.", "кость", 3, 6, "Шарик", 9),
+                new Dog("в квартире.", "корм для собак", 8, 2, "Номи", 12),
+                new Dog("в квартире.", "кость", 6, 9, "Арчебальд", 14.5),
+                new Horse("в сарае.", "овес", 15, 450, "Скакун", 300)
         };
         for (Animals a : animals) {
             Vet.treatAnimal(a);
