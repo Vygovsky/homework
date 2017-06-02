6f758a705adb26440661b50dbb1b64e9098f4886
roman_v <vigmol@ukr.net>
2017-06-02 13:59:53 +0300

    Добалил

--- src/L10/task1/Main.java
+++ src/L10/task1/Main.java
@@ -5,7 +5,9 @@ package L10.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        Shape[] figura = {new Circle(3, 6, 5), new Rectangle(2, 6, 10, 3)};
+        Shape[] figura = {
+                new Circle(3, 6, 5),
+                new Rectangle(2, 6, 10, 3)};
         for (Shape shape : figura) {
             if (shape instanceof Circle) {
                 System.out.println("Выводим площадь круга: " + shape.area());
