e198542bc20f2258bc3d6beda4bc08761c596565
Roman_v <vigmol@ukr.net>
2017-06-05 21:39:03 +0300

    Готов lesson 9 task5

---
+++ src/L10/task5/Guitar.java
@@ -0,0 +1,14 @@
+package L10.task5;
+
+/**
+ * Created by Roman_v on 05.06.2017.
+ */
+public class Guitar implements Tool {
+    private int stringNumber;
+
+    @Override
+    public void play() {
+        System.out.println("Играет " + KEY + " гитара.");
+
+    }
+}
