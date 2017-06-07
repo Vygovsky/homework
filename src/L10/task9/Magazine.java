7f47d0aa55f7e189115db1f9961ba1ea5bc34472
roman_v <vigmol@ukr.net>
2017-06-07 13:15:31 +0300

    по фиксел урок 10 task9 #11

--- src/L10/task9/Magazine.java
+++ src/L10/task9/Magazine.java
@@ -34,6 +34,11 @@ public class Magazine implements Printable {
     }
 
     @Override
+    public String toString() {
+        return "Magazine{" + "name='" + name + '\'' + '}';
+    }
+
+    @Override
     public int hashCode() {
         return name.hashCode();
     }
