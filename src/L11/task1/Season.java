c72b40efe734f2795545ed4d3cd924da09c8f5cf
roman_v <vigmol@ukr.net>
2017-06-07 17:08:14 +0300

    добавил 11 task 1 i work

---
+++ src/L11/task1/Season.java
@@ -0,0 +1,18 @@
+package L11.task1;
+
+/**
+ * Created by roman_v on 07.06.17.
+ */
+enum Season {
+    WINTER(0), SPRING(10), SUMMER(23), AUTUMN(16);
+
+    Season(int temp) {
+        this.temp = temp;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    private int temp;
+}
