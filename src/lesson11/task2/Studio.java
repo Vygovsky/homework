bb3054d5f402c907aa31234c07b401a73bb5985d
roman_v <vigmol@ukr.net>
2017-06-12 11:06:41 +0300

    по фиксил lesson 11 task 2

--- src/lesson11/task2/Studio.java
+++ src/lesson11/task2/Studio.java
@@ -7,9 +7,18 @@ public class Studio {
     public void clotheMan(Clothes[] clothes) {
         for (Clothes clotheMan : clothes) {
             if (clotheMan instanceof MenClothes) {
-                System.out.println("Одеваем на мужщину " + clotheMan);
+                System.out.println("Одеваем на мужщину " + clotheMan.getClass().getSimpleName());
+            }
+        }
+    }
+
+    public void clotheWomen(Clothes[] clothes) {
+        for (Clothes clotheWomen : clothes) {
+            if (clotheWomen instanceof WomenClothes) {
+                System.out.println("Одеваем женщину " + clotheWomen.getClass().getSimpleName());
             }
         }
     }
 }
+
 
