d7c45821a7fe9186581d92838be5ea7329d18991
roman_v <vigmol@ukr.net>
2017-06-13 09:25:22 +0300

    lesson 12 task 1 in work

---
+++ src/lesson12/task1/MinNumbString.java
@@ -0,0 +1,11 @@
+package lesson12.task1;
+
+/**
+ * Created by roman_v on 13.06.17.
+ */
+public class MinNumbString {
+    public static void main(String[] args) {
+        StringBuilder sb;
+
+    }
+}
