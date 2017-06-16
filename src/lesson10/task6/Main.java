ddb1864f08deeb4e44bbbb29cfa2fc2f1d6b9d45
roman_v <vigmol@ukr.net>
2017-06-16 15:38:02 +0300

    Lesson 12. Task 5 in work in errors

--- src/lesson10/task6/Main.java
+++ src/lesson10/task6/Main.java
@@ -4,51 +4,50 @@ package lesson10.task6;
  * Created by Roman_v on 05.06.2017.
  */
 public class Main {
+    private static int apricot;
+
     public static void main(String[] args) {
-        Apple[] apple = {
+        Fruit[] fruits = {
                 new Apple(" Лигольд", 23),
                 new Apple(" Белый налив", 13.50),
-                new Apple(" Декоста", 15.20),
-                new Apple(" Ченный принц", 21.20),};
-        double appleCost = 0;
-        for (Apple fruit : apple) {
-            appleCost += fruit.cost();
-        }
-        System.out.println("Стоимость яблок " + appleCost);
-
-        Apricot[] apricot = {
                 new Apricot("Цунами", 55.30),
                 new Apricot("Ледана", 56.90),
-                new Apricot("Робада", 60),
-                new Apricot("Киото", 60.25)};
-        double apricCost = 0;
-        for (Apricot fruit : apricot) {
-            apricCost += fruit.cost();
-        }
-        System.out.println("Стоимоть абрикос " + apricCost);
-
-        Pear[] pear = {
-                new Pear("Бере боск", 66),
                 new Pear("Вильямс", 70.50),
                 new Pear("Велеса", 55)};
-        double pearCost = 0;
-        for (Pear fruit : pear) {
+
+    }
+
+    public static double pirntInfoApple(Fruit[] apple) {
+        double sum = 0;
+        for (Fruit x : fruits) {
+            sum += x.cost();
+            System.out.println("Стоимость яблок " + sum);
+        }
+
+
+
+       public static double printInfoApricot(Fruit[] apricot) {
+        double apricCost = 0;
+        for (Fruit fruits : apricot) {
+            apricCost += fruits.cost();
+            System.out.println("Стоимоть абрикос " + apricCost);
+        }
+       }
+
+        public static double printInfoPear(){
+
+        }
+           pearCost = 0;
+           for (Pear fruit : pear) {
             pearCost += fruit.cost();
         }
-        System.out.println("Стоимость груш "+pearCost);
+        System.out.println("Стоимость груш " + pearCost);
 
-    Fruit[]fruits={
-            new Apple(" Лигольд", 23),
-            new Apple(" Белый налив", 13.50),
-            new Apricot("Цунами", 55.30),
-            new Apricot("Ледана", 56.90),
-            new Pear("Вильямс", 70.50),
-            new Pear("Велеса", 55)};
-        double getCount=0;
+
         for (Fruit fruit : fruits) {
-            getCount+=fruit.cost();
+            getCount += fruit.cost();
         }
-        System.out.println("Стоимость всех фруктов "+getCount);
+        System.out.println("Стоимость всех фруктов " + getCount);
     }
 
 }
