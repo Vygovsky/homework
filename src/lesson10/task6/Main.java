e244781d3335c890114c4c3d4e79777690bd1452
Roman_v <vigmol@ukr.net>
2017-06-17 14:57:09 +0300

    lesson 10. Task 6 in work

--- src/lesson10/task6/Main.java
+++ src/lesson10/task6/Main.java
@@ -8,47 +8,41 @@ public class Main {
 
     public static void main(String[] args) {
         Fruit[] fruits = {
-                new Apple(" Лигольд", 23),
-                new Apple(" Белый налив", 13.50),
-                new Apricot("Цунами", 55.30),
-                new Apricot("Ледана", 56.90),
-                new Pear("Вильямс", 70.50),
-                new Pear("Велеса", 55)};
+            new Apple(" Лигольд", 23),
+            new Apple(" Белый налив", 13.50),
+            new Apricot("Цунами", 55.30),
+            new Apricot("Ледана", 56.90),
+            new Pear("Вильямс", 70.50),
+            new Pear("Велеса", 55)};
 
+        System.out.println("Всего продано фруктов "+Fruit.getCount());
     }
 
-    public static double pirntInfoApple(Fruit[] apple) {
+
+
+    public static double applesTotalCost(Fruit[] fruits) {
         double sum = 0;
-        for (Fruit x : fruits) {
-            sum += x.cost();
-            System.out.println("Стоимость яблок " + sum);
-        }
-
-
-
-       public static double printInfoApricot(Fruit[] apricot) {
-        double apricCost = 0;
-        for (Fruit fruits : apricot) {
-            apricCost += fruits.cost();
-            System.out.println("Стоимоть абрикос " + apricCost);
-        }
-       }
-
-        public static double printInfoPear(){
-
-        }
-           pearCost = 0;
-           for (Pear fruit : pear) {
-            pearCost += fruit.cost();
-        }
-        System.out.println("Стоимость груш " + pearCost);
-
-
         for (Fruit fruit : fruits) {
-            getCount += fruit.cost();
+            if (fruit instanceof Apple)
+                sum += fruit.cost();
         }
-        System.out.println("Стоимость всех фруктов " + getCount);
+        return sum;
     }
-
+    public static double apricotTotalCost(Fruit[] fruits) {
+        double sum = 0;
+        for (Fruit fruit : fruits) {
+            if (fruit instanceof Apricot)
+                sum += fruit.cost();
+        }
+        return sum;
+    }
+    public static double pearTotalCost(Fruit[] fruits) {
+        double sum = 0;
+        for (Fruit fruit : fruits) {
+            if (fruit instanceof Pear)
+                sum += fruit.cost();
+        }
+        return sum;
+    }
 }
 
