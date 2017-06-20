8d54a7b2df3736b7e346038b5a213fbb16c38208
roman_v <vigmol@ukr.net>
2017-06-20 16:18:58 +0300

    Lesson 12. Task1

--- src/lesson12/task1/MinNumbString.java
+++ src/lesson12/task1/MinNumbString.java
@@ -1,15 +1,24 @@
 package lesson12.task1;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 /**
  * Created by roman_v on 13.06.17.
  */
 public class MinNumbString {
-        public static void main(String[] args) {
-            String l="Найти слово, в котором число различных символов минимально. ";
+    public static void main(String[] args) {
+            String [] symbol={" Работа была хорошо встречена публикой и критиками."};
+            String symbol2=symbol[0];
+        for (int i = 0; i <symbol.length ; i++) {
+            System.out.println(symbol[i]+" ");
+            if(MinNumCHislo(symbol[i])<MinNumCHislo(symbol2));
+            symbol2=symbol[i];
+            System.out.println(" ");
+            System.out.println("dsfsdfs"+symbol+"dsgfsd"+MinNumCHislo(symbol2));
+        }
 
-            Matcher m= Pattern.compile("[A-Za-zА-Яа-я0-9]+").matcher(l);
+
+            //  String l="Найти слово, в котором число различных символов минимально. ";
+
+/*           Matcher m= Pattern.compile("[A-Za-zА-Яа-я0-9]+").matcher(l);
             Set<String> words=new LinkedHashSet<>();
             while (m.find()){
                 words.add(l.substring(m.start(),m.end()));
@@ -30,6 +39,19 @@ public class MinNumbString {
             for (int i = 0; i <word.length(); i++) {
                 letters.add(word.charAt(i));
             }
+        }*/
+    }
+
+    public static int MinNumCHislo(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            String st = String.valueOf(s.charAt(i));
+            if (s.length() == -1) {
+                sb.indexOf(st);
+                sb.append(st);
+            }
         }
+        return sb.length();
     }
 }
+
