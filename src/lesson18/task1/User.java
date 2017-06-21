e3c74188772f3ea30ee90dfffe4ccd9772821d92
mashasupereka <milton.crost.91@gmail.com>
2017-06-21 21:35:50 +0300

    по фиксил Lesson18 task1

--- src/lesson18/task1/User.java
+++ src/lesson18/task1/User.java
@@ -6,12 +6,24 @@ package lesson18.task1;
 public class User {
     private String login="Makar";
     private String password="115872";
-    class Query{
+    public void createQuery(){
+        Query q=new Query();
+        q.printToLog();
+    };
+  public class Query{
         public void printToLog(){
             System.out.println("User is "+User.this);
             System.out.println("login: "+login);
             System.out.println("password: "+password);
         }
+
+    }
+    public static void main(String[] args) {
+        User user=new User();
+        User.Query query= user.new Query();
+      //  User.Query query1=
+        user.createQuery();
+        query.printToLog();
 
     }
 }
