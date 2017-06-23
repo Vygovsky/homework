335bdaf790f61534ecd5c1a1ba966572dc516c04
Roman_v <vigmol@ukr.net>
2017-06-23 22:35:11 +0300

    по фиксил Lesson 10. Task 1 #13

--- src/lesson8/task4/animals/Animal.java
+++ src/lesson8/task4/animals/Animal.java
@@ -59,4 +59,39 @@ public class Animal {
     public void setWeight(double weight) {
         this.weight = weight;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Animal animal = (Animal) o;
+
+        if (ages != animal.ages) return false;
+        if (Double.compare(animal.weight, weight) != 0) return false;
+        if (location != null ? !location.equals(animal.location) : animal.location != null) return false;
+        return food != null ? food.equals(animal.food) : animal.food == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = location != null ? location.hashCode() : 0;
+        result = 31 * result + (food != null ? food.hashCode() : 0);
+        result = 31 * result + ages;
+        temp = Double.doubleToLongBits(weight);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+            "location='" + location + '\'' +
+            ", food='" + food + '\'' +
+            ", ages=" + ages +
+            ", weight=" + weight +
+            '}';
+    }
 }
