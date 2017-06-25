1ee285c1fd9dd9b9545f2eb71ba8a21aa6fcd7a7
hannad1601 <anna7dreeva@gmail.com>
2017-06-25 11:45:08 +0300

    11

--- src/lesson12/task4/Report.java
+++ src/lesson12/task4/Report.java
@@ -1,11 +1,16 @@
 package lesson12.task4;
 
+import java.util.Date;
+
 /**
  * Created by Roman_v on 15.06.2017.
  */
 public class Report {
     public static void generateReport(Employee[]employees) {
+        System.out.println("Имя зарплата дата ");
         for (Employee employee : employees) {
+            System.out.printf(" %s    %s    ",employee.getFullName(),employee.getSalary());
+            System.out.printf("%1$s %2$tB %2$td, %2$tY\n", "Дата:", new Date());
 
 
 
