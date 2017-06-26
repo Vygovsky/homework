f6e4ec7a42fcd5f451f28c2fe77e08a2007b487a
roman_v <vigmol@ukr.net>
2017-06-26 16:21:54 +0300

    Lesson 12 task1

--- src/lesson12/task1/MinNumbString.java
+++ src/lesson12/task1/MinNumbString.java
@@ -1,6 +1,8 @@
 package lesson12.task1;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,29 +11,42 @@ import java.util.regex.Pattern;
  */
 public class MinNumbString {
     public static void main(String[] args) {
-        String l = "Найти слово, в котором число различных символов минимально. ";
+        String l = "Найти слово, в котором число различных символов минимально.";
         Matcher m = Pattern.compile("[A-Za-zА-Яа-я0-9]+").matcher(l);
         Set<String> words = new LinkedHashSet<>();
         while (m.find()) {
             words.add(l.substring(m.start(), m.end()));
         }
-        printInfo(words);
+        System.out.println(getWord(words));
     }
 
     public static void printInfo(Set<String> words) {
         for (String word : words) {
             System.out.println(word);
+            System.out.print(getNumbChar(word));
         }
     }
 
-    public static void getLetter(Set<String> words) {
+    public static int getNumbChar(String word) {
         Set<Character> letters = new HashSet<>();
+        String s = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            letters.add(word.charAt(i));
+        }
+        return letters.size();
+    }
+
+    public static String getWord(Set<String> words) {
+        String res = " ";
+        int charNumb = Integer.MAX_VALUE;
         for (String word : words) {
-            String s = word.toLowerCase();
-            for (int i = 0; i < word.length(); i++) {
-                letters.add(word.charAt(i));
+            int j = getNumbChar(word);
+            if (j<charNumb) {
+                res = word;
             }
         }
+        return res;
     }
 }
+
 
