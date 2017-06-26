673350d0e4aca88521322ef1fde0cbbf222cd7d2
Roman_v <vigmol@ukr.net>
2017-06-26 20:25:28 +0300

    по фиксил Lesson12

--- src/lesson12/task4/DemoEmpoloyee.java
+++ src/lesson12/task4/DemoEmpoloyee.java
@@ -4,9 +4,14 @@ package lesson12.task4;
  * Created by UITS-Admin on 21.06.2017.
  */
 public class DemoEmpoloyee {
-    Employee[]employees={
-            new Employee("Маркович Андрей Николаевич",1500.0),
-            new Employee("Маркович Андрей Николаевич",1500.0),
-            new Employee("Маркович Андрей Николаевич",1500.0)
-    };
+    public static void main(String[] args) {
+        Employee[] employees = {
+            new Employee("Маркович Андрей Николаевич", 1500.0),
+            new Employee("Нинкович  Семен Петрович", 1800.0),
+            new Employee("Курица Светлана Бенедиктовна", 2500.0)
+        };
+        Report.generateReport(employees);
+    }
 }
+
+
--- src/lesson12/task4/Report.java
+++ src/lesson12/task4/Report.java
@@ -4,16 +4,17 @@ package lesson12.task4;
  * Created by Roman_v on 15.06.2017.
  */
 public class Report {
-    public static void generateReport(Employee[]employees) {
+    public static void generateReport(Employee[] employees) {
         for (Employee employee : employees) {
-
+            String[] fullName = employee.getFullName().split("\\s+");
+            System.out.printf(employee.getFullName());
 
 
         }
 
-
-        }
 
     }
+
+}
 
 
