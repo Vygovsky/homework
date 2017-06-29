efb0c36d2c5fe013df4a294a6059a95cb7a9c888
roman_v <vigmol@ukr.net>
2017-06-29 13:55:20 +0300

    Lesson 14 task2

--- src/lesson14/task2/ArrayDequeDemo.java
+++ src/lesson14/task2/ArrayDequeDemo.java
@@ -3,21 +3,22 @@ package lesson14.task2;
 import lesson14.task1.MyHeavyBox;
 
 import java.util.Deque;
+import java.util.ArrayDeque;
 
 /**
  * Created by roman_v on 26.06.17.
  */
-public class ArrayDeque {
+public class ArrayDequeDemo {
     public static void main(String[] args) {
-        Deque<MyHeavyBox> qBox = new ArrayDeque<MyHeavyBox>();
+        Deque<MyHeavyBox> qBox = new ArrayDeque<>();
 
         qBox.add(new MyHeavyBox());
         qBox.add(new MyHeavyBox());
         qBox.add(new MyHeavyBox());
         qBox.add(new MyHeavyBox());
 
-        while (!queue.isEmpty()) {
-            System.out.println(queue.remove() + " ");
+        while (!qBox.isEmpty()) {
+            System.out.println(qBox.remove() + " ");
         }
     }
 }
