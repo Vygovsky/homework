01ccac6d0b9752cd7717c2448f7f4cecfe8fe12a
roman_v <vigmol@ukr.net>
2017-06-30 15:18:03 +0300

    Lesson 14 task5

---
+++ src/lesson14/task5/Category.java
@@ -0,0 +1,34 @@
+package lesson14.task5;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Created by roman_v on 30.06.17.
+ */
+public class Category {
+    private String name;
+   SortedSet<Product> products=new TreeSet<>();
+
+
+    public Category(String name, SortedSet<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SortedSet<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(SortedSet<Product> products) {
+        this.products = products;
+    }
+}
