c26709bf04e2642a0d285b5488a6601e4f50ecb7
roman_v <vigmol@ukr.net>
2017-06-30 10:42:27 +0300

    Lesson 14 task3

--- src/lesson14/task3/MySet.java
+++ src/lesson14/task3/MySet.java
@@ -1,7 +1,7 @@
 package lesson14.task3;
 
-        import java.util.HashSet;
-        import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by roman_v on 26.06.17.
@@ -13,24 +13,39 @@ public class MySet {
         Set<Integer> set3 = new HashSet<>();
         Set<Integer> set4 = new HashSet<>();
 
-       set1.add(6);
-       set1.add(8);
-       set1.add(20);
-       set1.add(2);
-       set3.add(6);
-       set3.add(6);
-       set3.add(91);
-       set3.add(59);
-
-
-
-
+        set1.add(6);
+        set1.add(8);
+        set1.add(20);
+        set1.add(2);
+        set3.add(6);
+        set3.add(6);
+        set3.add(91);
+        set3.add(59);
+     //  union(set1).size();
+        set2.add(1);
+        set2.add(26);
+        set2.add(9);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(12);
+        set4.add(12);
+        set4.add(1);
+        set4.add(16);
+        set4.add(9);
+        set4.add(129);
+        set4.add(1111);
+    //    intersect()
     }
-    public static Set<Iterable> union (Set<Integer>set1,Set<Integer>set2){
-     return set1.addAll(set2);
+
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        set1.addAll(set2);
+        return set1;
     }
-    public static Set<Iterable> intersect (Set<Integer>set3,Set<Integer>set4){
-        return set3.retainAll(set4);
+
+    public static Set<Integer> intersect(Set<Integer> set3, Set<Integer> set4) {
+        set3.retainAll(set4);
+        return set3;
     }
 
 }
