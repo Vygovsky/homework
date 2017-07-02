dd88eda09d82bccb31bd8f630873fc99403ae1af
hannad1601 <anna7dreeva@gmail.com>
2017-07-02 11:27:42 +0300

    по фиксил lesson 10 task4

--- src/lesson10/task4/Main.java
+++ src/lesson10/task4/Main.java
@@ -8,11 +8,19 @@ import lesson10.task4.f.F;
  */
 public class Main {
     public static void main(String[] args) {
-        A a=new A();
-        B b=new B();
-        C c=new C();
-        D d=new D();
-        F f=new F();
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        F f = new F();
         a.printInfoA();
+        System.out.println();
+        b.printInfoB();
+        System.out.println();
+        c.printInfoC();
+        System.out.println();
+        d.printInfoD();
+        System.out.println();
+        f.printInfoF();
     }
 }
