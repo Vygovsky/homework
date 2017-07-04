6c103a7034a10bdf7d0cfb1094d0cc4773368dd2
roman_v <vigmol@ukr.net>
2017-07-04 10:08:34 +0300

    to fix Lesson 12 task4

--- src/lesson12/task4/FullReport.java
+++ src/lesson12/task4/FullReport.java
@@ -1,12 +1,23 @@
 package lesson12.task4;
 
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Created by roman_v on 26.06.17.
  */
 public class FullReport extends Report {
+    private static void printInfo(String country, String language) throws UnsupportedEncodingException {
+        Locale locale = new Locale(country, language);
+        ResourceBundle rb = ResourceBundle.getBundle("text", locale);
+        String s = rb.getString("text4");
+        s = new String(s.getBytes("ISO-8859-1"), "UTF-8");
+        System.out.println(s);
+    }
+
     public static void generateReport(Employee[] employees) {
         NumberFormat nf = NumberFormat.getInstance();
         nf.setMaximumFractionDigits(2);
