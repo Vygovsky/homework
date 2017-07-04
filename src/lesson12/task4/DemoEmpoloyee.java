1bdaaeef37e2293f568f28aad1cbdbd0f2ead85c
roman_v <vigmol@ukr.net>
2017-07-04 10:05:31 +0300

    to fix Lesson 12 task4

--- src/lesson12/task4/DemoEmpoloyee.java
+++ src/lesson12/task4/DemoEmpoloyee.java
@@ -1,17 +1,20 @@
 package lesson12.task4;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 /**
  * Created by UITS-Admin on 21.06.2017.
  */
 public class DemoEmpoloyee {
-    public static void main(String[] args) {
-        Employee[]employees={
-            new Employee("Маркович Андрей Николаевич",1500.0/27, new Date()),
-            new Employee("Марк Андрей Николаевич",1500.0/51, new Date()),
-            new Employee("Ма Андрей Николаевич",1500.0/87, new Date())
-        };
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        Employee[] employees = {
+                new Employee("Маркович Андрей Николаевич", 1500.0 / 21, new Date()),
+                new Employee("Марк Андрей Николаевич", 1800.0 / 21, new Date()),
+                new Employee("Ромбик Андрей Николаевич", 2000.0 / 21, new Date())};
         FullReport.generateReport(employees);
+
     }
+
 }
