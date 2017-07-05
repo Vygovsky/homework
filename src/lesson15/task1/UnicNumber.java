84dd648a6898e34b228a92c613d06f7ae9edcdcf
mashasupereka <milton.crost.91@gmail.com>
2017-07-05 18:40:27 +0300

    to fix Lesson15 task1

---
+++ src/lesson15/task1/UnicNumber.java
@@ -0,0 +1,25 @@
+package lesson15.task1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * Created by UITS-Admin on 05.07.2017.
+ */
+public class UnicNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        if(sc.hasNext()){
+            String[] numbers = sc.nextLine().split("\\s");
+            Set<String> num=new HashSet<>();
+            for (String number : numbers) {
+                num.add(number);
+            }
+            for (String s : num) {
+                System.out.println(s);
+            }
+        }
+
+    }
+}
