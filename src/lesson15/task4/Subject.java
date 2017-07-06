b31359aa22e5cbbcffb114cb809cc680e9a79c5d
roman_v <vigmol@ukr.net>
2017-07-06 16:19:33 +0300

    to fix Lesson 15 task4

--- src/lesson15/task4/Subject.java
+++ src/lesson15/task4/Subject.java
@@ -1,12 +1,18 @@
 package lesson15.task4;
 
+        import java.util.Arrays;
         import java.util.List;
 
 /**
  * Created by roman_v on 06.07.17.
  */
 public enum Subject {
-    MATHEMATICS(1, 2, 3), PHYSICS(1, 2, 3), DRAWING(1, 2), ENGINE_STRUCTURE(1, 2, 3, 4, 5), TRANSMISSION(3, 4), ELECTRONICS(2, 3, 4);
+    MATHEMATICS(Arrays.asList(1,2,3)),
+    PHYSICS(Arrays.asList(1,2,3)),
+    DRAWING(Arrays.asList(2,3)),
+    ENGINE_STRUCTURE(Arrays.asList(1, 2, 3, 4, 5)),
+    TRANSMISSION(Arrays.asList(3, 4)),
+    ELECTRONICS(Arrays.asList(3,4,5));
     public List<Integer> courses;
 
     Subject(List<Integer> courses) {
