3161ac87808aa8bec608014f9d26cb9b3d9c865b
roman_v <vigmol@ukr.net>
2017-07-06 11:45:53 +0300

    to fix Lesson 15 task1

--- src/lesson15/task1/UnicNumber.java
+++ src/lesson15/task1/UnicNumber.java
@@ -9,17 +9,16 @@ import java.util.Set;
  */
 public class UnicNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        if(sc.hasNext()){
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNext()) {
             String[] numbers = sc.nextLine().split("[\\s,]+");
-            Set<String> num=new HashSet<>();
+            Set<String> num = new HashSet<>();
             for (String number : numbers) {
                 num.add(number);
             }
             for (String s : num) {
-                System.out.print(s+" ");
+                System.out.print(s + " ");
             }
         }
-
     }
 }
