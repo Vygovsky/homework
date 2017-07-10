d31e51b707ed6b8b103181fc66773d0ddcfb34b5
roman_v <vigmol@ukr.net>
2017-07-10 13:51:18 +0300

    to fix Lesson 18 task1

--- src/lesson18/task1/User.java
+++ src/lesson18/task1/User.java
@@ -4,25 +4,26 @@ package lesson18.task1;
  * Created by roman_v on 21.06.17.
  */
 public class User {
-    private String login="Makar";
-    private String password="115872";
-    public void createQuery(){
-        Query q=new Query();
+    private String login = "Makar";
+    private String password = "115872";
+
+    public void createQuery() {
+        Query q = new Query();
         q.printToLog();
-    };
-  public class Query{
-        public void printToLog(){
-            System.out.println("User is "+User.this);
-            System.out.println("login: "+login);
-            System.out.println("password: "+password);
+    }
+
+    public class Query {
+        public void printToLog() {
+            System.out.println("User is " + User.this);
+            System.out.println("login: " + login);
+            System.out.println("password: " + password);
         }
     }
     public static void main(String[] args) {
-        User user=new User();
-        User.Query query= user.new Query();
-      //  User.Query query1=
+        User user = new User();
+        User.Query query = user.new Query();
+        //  User.Query query1=
         user.createQuery();
         query.printToLog();
-
     }
 }
