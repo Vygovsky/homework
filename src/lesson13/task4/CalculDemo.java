773002b14d3a6cb05a2a52447adfc79cc56afea2
roman_v <vigmol@ukr.net>
2017-07-10 15:55:31 +0300

    to fix Lesson 13 task4

---
+++ src/lesson13/task4/CalculDemo.java
@@ -0,0 +1,11 @@
+package lesson13.task4;
+
+/**
+ * Created by roman_v on 10.07.17.
+ */
+public class CalculDemo {
+    public static void main(String[] args) {
+        System.out.println(Calkulator.sum((byte)127, (short)5));
+
+    }
+}
--- src/lesson13/task4/Calkulator.java
+++ src/lesson13/task4/Calkulator.java
@@ -6,18 +6,18 @@ package lesson13.task4;
  */
 public class Calkulator {
     public static <T extends Number, V extends Number> double sum(T t, V v) {
-        return (int) t.doubleValue() + v.doubleValue();
+        return  t.doubleValue() + v.doubleValue();
     }
 
     public static <T extends Number, V extends Number> double minus(T t, V v) {
-        return (int) t.doubleValue() - v.doubleValue();
+        return t.doubleValue() - v.doubleValue();
     }
 
     public static <T extends Number, V extends Number> double generation(T t, V v) {
-        return (int) t.doubleValue() * v.doubleValue();
+        return  t.doubleValue() * v.doubleValue();
     }
 
     public static <T extends Number, V extends Number> double division(T t, V v) {
-        return (int) t.doubleValue() / v.doubleValue();
+        return t.doubleValue() / v.doubleValue();
     }
 }
