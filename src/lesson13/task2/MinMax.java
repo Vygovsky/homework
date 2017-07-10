67ebcf2acce55fa2840501e788c346651cc1e735
roman_v <vigmol@ukr.net>
2017-07-10 13:55:24 +0300

    to fix Lesson 13 task2

--- src/lesson13/task2/MinMax.java
+++ src/lesson13/task2/MinMax.java
@@ -3,32 +3,31 @@ package lesson13.task2;
 /**
  * Created by roman_v on 26.06.17.
  */
-public class MinMax <T extends Number> {
-   private T[]arr;
+public class MinMax<T extends Number> {
+    private T[] arr;
 
     public MinMax(T[] arr) {
         this.arr = arr;
     }
 
-    public  double minNumb(){
-        T min=arr[0];
-        for (T i:arr) {
-        if(i.doubleValue()<min.doubleValue()){}
+    public double minNumb() {
+        T min = arr[0];
+        for (T i : arr) {
+            if (i.doubleValue() < min.doubleValue()) {
+            }
             min = i;
         }
         return min.doubleValue();
-        }
-    public  double maxNumb(){
-        T max=arr[0];
-        for (T i:arr) {
-            if(i.doubleValue()>max.doubleValue()){}
+    }
+
+    public double maxNumb() {
+        T max = arr[0];
+        for (T i : arr) {
+            if (i.doubleValue() > max.doubleValue()) {
+            }
             max = i;
         }
         return max.doubleValue();
     }
-
-
-
-
 }
 
--- src/lesson13/task2/MinMaxDemo.java
+++ src/lesson13/task2/MinMaxDemo.java
@@ -5,13 +5,11 @@ package lesson13.task2;
  */
 public class MinMaxDemo {
     public static void main(String[] args) {
-        Integer[] intNumb=new Integer[]{18,-5,6,20,-30};
-        Double[]dNumb=new Double[]{22.6,229.3,-6.5,0.63};
-        MinMax <Integer> minMax1=new MinMax<>(intNumb);
-        MinMax<Double>minMax2=new MinMax<>(dNumb);
+        Integer[] intNumb = new Integer[]{18, -5, 6, 20, -30};
+        Double[] dNumb = new Double[]{22.6, 229.3, -6.5, 0.63};
+        MinMax<Integer> minMax1 = new MinMax<>(intNumb);
+        MinMax<Double> minMax2 = new MinMax<>(dNumb);
         System.out.println(minMax1.minNumb());
         System.out.println(minMax2.minNumb());
-
-
     }
 }
