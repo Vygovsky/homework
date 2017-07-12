18a6e3cb640d6960bf817ee58789b3515f820147
roman_v <vigmol@ukr.net>
2017-07-12 11:38:20 +0300

    to fix Lesson 13 task3

--- src/lesson13/task3/Matrix2.java
+++ src/lesson13/task3/Matrix2.java
@@ -5,6 +5,15 @@ package lesson13.task3;
  */
 public class Matrix2 {
     public static void main(String[] args) {
+        Integer[][] iMatrix = new Integer[][]{{-50, 2, 23,}, {19, 6, -8}};
+        Double[][] dMatrix = new Double[][]{{53.9, -2.3, 62.0,}, {28.3, 0.3, -5.0,}};
+        Matrix <Integer> integerMatrix=new Matrix<>(iMatrix);
+        Matrix <Double>doubleMatrix=new Matrix<>(dMatrix);
+        Matrix sum=integerMatrix.sum(integerMatrix);
+        Matrix multiply = doubleMatrix.multiply(doubleMatrix);
+        System.out.println(multiply);
+        System.out.println();
+        System.out.println(sum);
 
 
     }
