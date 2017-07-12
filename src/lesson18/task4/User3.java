d9e7cf2d225712c3cb6355ae921fc9a6790d31c3
mashasupereka <milton.crost.91@gmail.com>
2017-07-12 18:33:50 +0300

    to fix Lesson18 task4

---
+++ src/lesson18/task4/User3.java
@@ -0,0 +1,37 @@
+package lesson18.task4;
+
+/**
+ * Created by roman_v on 21.06.17.
+ */
+public class User3 {
+    private String login = "Makar";
+    private String password = "115872";
+
+    public void createQuery() {
+        User3.Query q = new User3.Query();
+        q.printToLog(this);
+    }
+
+    public static class Query {
+        public void printToLog(User3 user) {
+            System.out.println("User is " + user);
+            System.out.println("login: " + user.login);
+            System.out.println("password: " + user.password);
+        }
+    }
+    public static void main(String[] args) {
+     User3 user = new User3();
+        user.createQuery();
+        User3.Query query = new User3.Query();
+        query.printToLog(user);
+        new User3.Query().printToLog(user);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
