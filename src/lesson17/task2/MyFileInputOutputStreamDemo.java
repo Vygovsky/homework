ec6de4deaf56a06177262e0ae359b879b73d13fc
roman_v <vigmol@ukr.net>
2017-07-14 15:42:23 +0300

    to fix Lesson 17 task2

--- src/lesson17/task2/MyFileInputOutputStreamDemo.java
+++ src/lesson17/task2/MyFileInputOutputStreamDemo.java
@@ -7,7 +7,7 @@ import java.io.*;
  */
 public class MyFileInputOutputStreamDemo {
     public static void main(String args[]) {
-        try(OutputStream output = new FileOutputStream("file.txt")) {
+        try(OutputStream output = new FileOutputStream("src\\lesson17\\task2\\file.txt")) {
             char c[] = {'a', 'b', 'c'};
             for (char c1:c) {
                 output.write(c1); // Запись каждого символа в текстовый файл
