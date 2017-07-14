04e818d567d8b4ab378ec630a31e6a1cb07b536a
roman_v <vigmol@ukr.net>
2017-07-14 16:07:23 +0300

    to fix Lesson 18 task1

--- src/lesson18/task1/User.java
+++ src/lesson18/task1/User.java
@@ -19,6 +19,7 @@ public class User {
             System.out.println("password: " + password);
         }
     }
+
     public static void main(String[] args) {
         User user = new User();
         user.createQuery();
@@ -30,18 +31,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+        return "User{" + "login='" + login + '\'' + ", password='" + password + '\'' + '}';
     }
 }
-/*
-* Создать класс User, содержащий private переменные login, password. Создать внутренний класс
- * Query в классе User. Класс Query содержит метод printToLog(), который распечатывает на консоль
-  * сообщение о том что пользователь с таким то логином и паролем отправил запрос. Класс User,
-  * содержит метод createQuery(), в котором создается объект класса Query и вызывается метод printToLog().
-   * В методе main(): создать экземпляр класса User и вызвать метод createQuery(); создать экземпляр класс
-    * Query и вызвать метод printToLog() используя конструкцию user.new Query(); создать экземпляр класса Query
-     * и вызвать метод printToLog() используя конструкцию new User().new Query();
-* */
