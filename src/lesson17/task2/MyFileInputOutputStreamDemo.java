10fdf1f3fa7800e1526690e681a0e9c714d5d0bd
roman_v <vigmol@ukr.net>
2017-07-14 15:39:03 +0300

    to fix Lesson 17 task2

---
+++ src/lesson17/task2/MyFileInputOutputStreamDemo.java
@@ -0,0 +1,30 @@
+package lesson17.task2;
+
+import java.io.*;
+
+/**
+ * Created by roman_v on 14.07.17.
+ */
+public class MyFileInputOutputStreamDemo {
+    public static void main(String args[]) {
+        try(OutputStream output = new FileOutputStream("file.txt")) {
+            char c[] = {'a', 'b', 'c'};
+            for (char c1:c) {
+                output.write(c1); // Запись каждого символа в текстовый файл
+            }
+            output.close();
+
+            InputStream input = new FileInputStream("file.txt");
+            int size = input.available();
+
+            for (int j = 0; j < size; j++) {
+                System.out.print((char) input.read() + " "); // Чтение текстового файла посимвольно
+            }
+            input.close();
+        } catch (IOException e) {
+            System.out.print("Exception");
+        }
+    }
+}
+
+
