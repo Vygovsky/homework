b92647e88ba70eac894fcbf0f06a875d44291f3c
roman_v <vigmol@ukr.net>
2017-07-14 15:17:53 +0300

    to fix Lesson 17 task1

--- src/lesson17/task1/MyCopyFile.java
+++ src/lesson17/task1/MyCopyFile.java
@@ -8,27 +8,15 @@ import java.io.IOException;
  * Created by roman_v on 14.07.17.
  */
 public class MyCopyFile {
-    public static void main(String args[]) throws IOException {
-        FileInputStream fileIn = null;
-        FileOutputStream fileOut = null;
-
-        try {
-            fileIn = new FileInputStream("\\src\\lesson17\\task1\\file.txt");
-            fileOut = new FileOutputStream("\\src\\lesson17\\task1\\copied_file.txt");
-
+    public static void main(String args[]) {
+        try (FileInputStream fIn = new FileInputStream("src\\lesson17\\task1\\file.txt");
+             FileOutputStream fOut = new FileOutputStream("src\\lesson17\\task1\\copied_file.txt")) {
             int a;
-            // Копирование содержимого файла file.txt
-            while ((a = fileIn.read()) != -1) {
-                fileOut.write(a); // Чтение содержимого файла file.txt и запись в файл copied_file.txt
+            while ((a = fIn.read()) != -1) {
+                fOut.write(a);
             }
-        } finally {
-            if (fileIn != null) {
-                fileIn.close();
-            }
-            if (fileOut != null) {
-                fileOut.close();
-            }
+        } catch (IOException e) {
+            System.out.println();
         }
     }
 }
-
