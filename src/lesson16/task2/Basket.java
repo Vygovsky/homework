28e71543907d968ce3ea37f11bff9341669d3ac8
mashasupereka <milton.crost.91@gmail.com>
2017-07-19 21:33:51 +0300

    to fix lesson16 task2

--- src/lesson16/task2/Basket.java
+++ src/lesson16/task2/Basket.java
@@ -1,8 +1,26 @@
 package lesson16.task2;
 
+import java.util.Map;
+
 /**
  * Created by roman_v on 18.07.17.
  */
 public class Basket {
+    private Map<Goods,Integer> goodMap;
 
+    public Basket(Map<Goods, Integer> goodMap) {
+        this.goodMap = goodMap;
+    }
+
+    public void addGoods(Goods goods){
+        goodMap.put(goods,1);
+        if (goodMap.containsKey(goods)) {
+            int count=goodMap.get(goods);
+            count++;
+
+        }
+    }
+    public void isEmpty(Goods goods){
+
+    }
 }
---
+++ src/lesson16/task2/Goods.java
@@ -0,0 +1,7 @@
+package lesson16.task2;
+
+/**
+ * Created by UITS-Admin on 19.07.2017.
+ */
+public class Goods {
+}
