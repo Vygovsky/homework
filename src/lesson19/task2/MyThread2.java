167b559f2316ad3d794ca681b3c17ca21ac8450f
mashasupereka <milton.crost.91@gmail.com>
2017-07-19 18:29:57 +0300

    to fix lesson19 task2

--- src/lesson19/task2/MyThread2.java
+++ src/lesson19/task2/MyThread2.java
@@ -6,13 +6,16 @@ public class MyThread2 extends Thread {
     public MyThread2(StringBuilder sb) {
         this.sb = sb;
     }
-@Override
-    public synchronized void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.print(sb);
+
+    @Override
+    public void run() {
+        synchronized (sb) {
+            for (int i = 0; i < 100; i++) {
+                System.out.print(sb);
+            }
+            System.out.println();
+            char ch = sb.charAt(0);
+            sb.setCharAt(0, ++ch);
         }
-        System.out.println();
-        char ch=sb.charAt(0);
-        sb.setCharAt(0,++ch);
     }
 }
