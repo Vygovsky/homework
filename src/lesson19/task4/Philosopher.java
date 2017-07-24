33ecd0d242716c7e4939e5d4b09177cb436ed947
Roman_v <vigmol@ukr.net>
2017-07-24 23:18:14 +0300

    to fix lesson19 task4

---
+++ src/lesson19/task4/Philosopher.java
@@ -0,0 +1,26 @@
+package lesson19.task4;
+
+/**
+ * Created by Roman_v on 24.07.2017.
+ */
+public class Philosopher implements Runnable {
+    private int number;
+    private Fork fork1;
+    private Fork fork2;
+
+    public Philosopher(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+   public void think(){
+
+   }
+   public void dropFork(){
+
+   }
+}
