a93e8b0ba7004b2219463f65ba52c524877fe461
Roman_v <vigmol@ukr.net>
2017-07-24 21:03:01 +0300

    to fix lesson19 task5

---
+++ src/lesson19/task5/ClassFlower.java
@@ -0,0 +1,18 @@
+package lesson19.task5;
+
+import lesson8.flowers.Flower;
+
+import java.util.Arrays;
+
+/**
+ * Created by Roman_v on 24.07.2017.
+ */
+public class ClassFlower {
+    public static void main(String[] args) {
+        Class flower= Flower.class;
+        System.out.println(flower.getClass());
+        System.out.println(Arrays.toString(flower.getDeclaredConstructors()));
+        System.out.println(Arrays.toString(flower.getDeclaredMethods()));
+        System.out.println(Arrays.toString(flower.getDeclaredFields()));
+    }
+}
