3a64240d7354747233ef37660e2ea2c07b50e60a
Roman_v <vigmol@ukr.net>
2017-07-24 23:16:57 +0300

    to fix lesson19 javadoc

---
+++ src/lesson19/task4/Fork.java
@@ -0,0 +1,18 @@
+package lesson19.task4;
+
+/**
+ * Created by Roman_v on 24.07.2017.
+ */
+public class Fork {
+
+    public void takeForkLeft() {
+        System.out.println("Философ берет вилку левой рукой");
+    }
+    public void takeForkRight() {
+        System.out.println("Философ берет вилку правой рукой");
+    }
+
+    public void dropFork() {
+
+    }
+}
