7f69dd90015f581c1cb0d3a7e364386d71531874
roman_v <vigmol@ukr.net>
2017-07-25 13:23:00 +0300

    to fix lesson19 task5

--- src/lesson19/task5/ClassFlower.java
+++ src/lesson19/task5/ClassFlower.java
@@ -2,6 +2,7 @@ package lesson19.task5;
 
 import lesson8.flowers.Flower;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 /**
@@ -10,10 +11,14 @@ import java.util.Arrays;
 public class ClassFlower {
     public static void main(String[] args) {
         Class flower = Flower.class;
-        System.out.println(flower.getClass());
-        System.out.println(Arrays.toString(flower.getDeclaredClasses()));
+        System.out.println(flower.getName());
+        System.out.println("saff "+Arrays.toString(flower.getDeclaredClasses()));
         System.out.println(Arrays.toString(flower.getDeclaredConstructors()));
-        System.out.println(Arrays.toString(flower.getDeclaredMethods()));
         System.out.println(Arrays.toString(flower.getDeclaredFields()));
+        System.out.println(Arrays.toString(flower.getDeclaredMethods()));
+        for (Method flow : flower.getDeclaredMethods()) {
+            System.out.println(flow);
+
+        }
     }
 }
