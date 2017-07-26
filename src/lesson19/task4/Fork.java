9d72e59bba64aaf491ed6ffc853b10f1b6ec3a1a
roman_v <vigmol@ukr.net>
2017-07-26 13:41:28 +0300

    to fix lesson19 task4

--- src/lesson19/task4/Fork.java
+++ src/lesson19/task4/Fork.java
@@ -4,9 +4,20 @@ package lesson19.task4;
  * Created by Roman_v on 24.07.2017.
  */
 public class Fork {
+    private Philosopher philosopher;
 
-    public void takeFork() {
-        System.out.println("Философ берет вилку рукой");
+    public synchronized void takeFork() {
+        philosopher.eat();
+        while (true) {
+            try {
+                wait(2000);
+                System.out.println("Ест " + philosopher);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        philosopher = null;
+        notify();
     }
 
     public void dropFork() {
