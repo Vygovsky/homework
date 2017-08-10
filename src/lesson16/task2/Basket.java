798385d0b4fee32dea33b33edc3a637a75816691
vygovsky <vigmol@ukr.net>
2017-08-10 23:14:30 +0300

    to fix lesson15 task5

--- src/lesson16/task2/Basket.java
+++ src/lesson16/task2/Basket.java
@@ -1,11 +1,27 @@
 package lesson16.task2;
 
+import lesson17.task3.Horse;
+
+import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Basket {
+public class Basket implements Serializable {
     private Map<Good, Integer> goodMap;
 
+    public Basket() {
+        goodMap=new HashMap<>();
+    }
+
     public Basket(Map<Good, Integer> goodMap) {
+        this.goodMap = goodMap;
+    }
+
+    public Map<Good, Integer> getGoodMap() {
+        return goodMap;
+    }
+
+    public void setGoodMap(Map<Good, Integer> goodMap) {
         this.goodMap = goodMap;
     }
 
@@ -23,4 +39,29 @@ public class Basket {
         }
     }
 
+    public void serialize(String fileName) {
+        try (FileOutputStream fs = new FileOutputStream(fileName);
+             ObjectOutputStream os = new ObjectOutputStream(fs)) {
+            os.writeObject(this);
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+    public void deserialize(String fileName){
+        try (FileInputStream fis = new FileInputStream(fileName);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            this.goodMap = ((Basket) ois.readObject()).getGoodMap();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Basket{" +
+                "goodMap=" + goodMap +
+                '}';
+    }
 }
--- src/lesson16/task2/Good.java
+++ src/lesson16/task2/Good.java
@@ -2,23 +2,22 @@ package lesson16.task2;
 
 import lesson11.task2.Size;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Good {
+public class Good implements Serializable {
     private int id;
     private String name;
-    private List<Size> sizes = new ArrayList<>();
+    private Size size;
 
-    public Good(int id, String name, Size... sizes) {
-        this.id = id;  // делать уникальнім
+    public Good(int id, String name, Size size) {
+        this.id = id;
         this.name = name;
-
-        for (Size size : sizes) {
-            this.sizes.add(size);
-        }
+        this.size = size;
     }
+
 
     public int getId() {
         return id;
@@ -36,12 +35,40 @@ public class Good {
         this.name = name;
     }
 
-    public List<Size> getSizes() {
-        return sizes;
+    public Size getSize() {
+        return size;
     }
 
-    public void setSizes(List<Size> sizes) {
-        this.sizes = sizes;
+    public void setSize(Size size) {
+        this.size = size;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Good good = (Good) o;
+
+        if (id != good.id) return false;
+        if (!name.equals(good.name)) return false;
+        return size == good.size;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + size.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Good{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                '}';
+    }
 }
