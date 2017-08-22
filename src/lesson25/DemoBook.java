c4f147b7785379e3fcc6b205f3082bfd572423c2
roman_v <vigmol@ukr.net>
2017-08-22 16:14:38 +0300

    to fix lesson25

--- src/lesson25/DemoBook.java
+++ src/lesson25/DemoBook.java
@@ -11,7 +11,7 @@ public class DemoBook {
         books.add(new Book(70, "Ant", "Tom Cruze", 326, Genre.CHILDRENS_LITERATURE));
         books.add(new Book(42, "Hobbits", "Lep Leprecon", 198, Genre.POEMS));
         books.add(new Book(38, "Last Blod", "Chak Noris", 1060, Genre.DISPLAYS));
-        books.add(new Book(105, "Rembo", "Rich Hak", 500, Genre.CLASSIC));
+        books.add(new Book(105, "Rembo", "Rich Ortis", 500, Genre.CLASSIC));
 
     }
 }
